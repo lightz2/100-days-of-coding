f6c0a01bf937b69952a573d9203ed6980402654a
fearlessserver <senatoreniola@gmail.com>
2023-07-22 21:10:38 +0100

    daily projects

---
+++ BanNamGen.py
@@ -0,0 +1,25 @@
+print("Welcome to band name generator.\n")
+city = input("Which city do you grow up in? \n")
+# am asking for any pet the user knows
+pet = input("name any pet name you know \n")
+# am adding the city with the pet name to generate a band name
+
+# unknown = city
+# city = pet
+# pet = unknown
+
+print("Your band name can be " + city + pet + "\n")
+
+
+
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print( fruit + " Pie")
+# print(fruits)
+
+# for loop with range
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total)
