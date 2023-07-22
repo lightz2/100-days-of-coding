d663d7a72a96b4f4f642b4eddce700d377332df4
fearlessserver <senatoreniola@gmail.com>
2023-07-22 21:10:09 +0100

    daily projects

---
+++ Tip calculator.py
@@ -0,0 +1,5 @@
+print("Welcome to Tip Calculator\n")
+# asking for the amount
+initical_amount = (input("How much do you want to pay"))
+# asking for the number of people to share the money
+num_people = input("How many people is spliting the money? \n:")
